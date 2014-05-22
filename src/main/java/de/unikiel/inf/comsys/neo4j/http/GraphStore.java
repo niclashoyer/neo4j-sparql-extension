c671cb32faab86d25dd824650fa0678de340e24f
Niclas Hoyer <niclas@verbugt.de>
2014-05-22 17:21:23 +0200

    Added SPARQL1.1 Graph Store protocol compliance (placeholder)

--- src/main/java/de/unikiel/inf/comsys/neo4j/http/GraphStore.java
+++ src/main/java/de/unikiel/inf/comsys/neo4j/http/GraphStore.java
@@ -5,7 +5,12 @@ import de.unikiel.inf.comsys.neo4j.rio.SailsRDFHandler;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import org.openrdf.model.Resource;
@@ -20,9 +25,16 @@ public class GraphStore extends AbstractSailsResource {
 		super(sc, vf);
 	}
 	
+	@GET
+	public Response graphIndirectGet(
+			@QueryParam("graph") String graphString,
+			@QueryParam("default") String def) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+	
 	@PUT
 	@Consumes("text/turtle")
-	public Response graph(
+	public Response graphIndirectPut(
 			@QueryParam("graph") String graphString,
 			@QueryParam("default") String def,
 			InputStream in) {
@@ -45,6 +57,51 @@ public class GraphStore extends AbstractSailsResource {
 			return Response.status(500).entity(
 					str.getBytes(Charset.forName("UTF-8"))).build();
 		}
+	}
+	
+	@DELETE
+	public Response graphIndirectDelete(
+			@QueryParam("graph") String graphString,
+			@QueryParam("default") String def) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+	
+	@POST
+	public Response graphIndirectPost(
+			@QueryParam("graph") String graphString,
+			@QueryParam("default") String def,
+			InputStream in) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+	
+	@GET
+	@Path("/{graph}")
+	public Response graphDirectGet(
+			@PathParam("graph") String graphString) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+	
+	@PUT
+	@Path("/{graph}")
+	public Response graphDirectPut(
+			@PathParam("graph") String graphString,
+			InputStream in) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+	
+	@DELETE
+	@Path("/{graph}")
+	public Response graphDirectDelete(
+			@PathParam("graph") String graphString) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+	
+	@POST
+	@Path("/{graph}")
+	public Response graphDirectPost(
+			@PathParam("graph") String graphString,
+			InputStream in) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
 	}
 	
 }
