5f17ea7d7540af7e9dd2d8453375158a8fe6dbc0
Niclas Hoyer <niclas@verbugt.de>
2014-05-22 14:45:43 +0200

    Added SPARQL1.1 Protocol update compliance (placeholder)

--- src/main/java/de/unikiel/inf/comsys/neo4j/http/SPARQLUpdate.java
+++ src/main/java/de/unikiel/inf/comsys/neo4j/http/SPARQLUpdate.java
@@ -1,6 +1,37 @@
 
 package de.unikiel.inf.comsys.neo4j.http;
 
-public class SPARQLUpdate {
+import java.io.InputStream;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.sail.SailConnection;
+
+public class SPARQLUpdate extends AbstractSailsResource {
 	
+	public SPARQLUpdate(SailConnection sc, ValueFactory vf) {
+		super(sc, vf);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response updatePOSTEncoded(
+			@FormParam("update") String query,
+			@FormParam("using-graph-uri") List<String> defgraphs,
+			@FormParam("using-named-graph-uri") List<String> namedgraphs) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
+
+	@POST
+	@Consumes("application/sparql-update")
+	public Response updatePOSTDirect(
+			@FormParam("using-graph-uri") List<String> defgraphs,
+			@FormParam("using-named-graph-uri") List<String> namedgraphs,
+			InputStream query) {
+		return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+	}
 }
