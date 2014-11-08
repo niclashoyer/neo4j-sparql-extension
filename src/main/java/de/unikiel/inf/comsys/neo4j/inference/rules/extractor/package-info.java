5c920db4a1b34bbbd78d167d588dd0b9b6d0aded
Niclas Hoyer <niclas@verbugt.de>
2014-11-08 20:25:32 +0100

    Added missing package informations

---
+++ src/main/java/de/unikiel/inf/comsys/neo4j/inference/rules/extractor/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Implementations of extractors that extract rules from an OWL-2 ontology.
+ */
+package de.unikiel.inf.comsys.neo4j.inference.rules.extractor;
---
+++ src/main/java/de/unikiel/inf/comsys/neo4j/inference/sail/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Subclasses of sesame queries that allow for creation of new queries from
+ * a tuple expression.
+ */
+package de.unikiel.inf.comsys.neo4j.inference.sail;
